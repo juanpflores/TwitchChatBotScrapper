c0b6cfc7422915af32640c65dcaf7b5a0b704958
Juan Pablo Flores <jp@juanpabloflores.me>
2017-01-12 16:08:34 -0600

    Applied PEP8

--- libs/botParser.py
+++ libs/botParser.py
@@ -12,9 +12,9 @@ file_name = ""
 
 
 def createOutputFile(channel_name):
-	''' Creates a Json output File on the sample folder 
-	that cointains the general information about the 
-	channel that includes channel name and the time 
+	''' Creates a PICKLE output File on the sample folder
+	that cointains the general information about the
+	channel that includes channel name and the time
 	that the channel began being scrapped. '''
 	global file_name
 	start_time = str(datetime.datetime.now()).split(".")[0]
@@ -34,16 +34,19 @@ def createOutputFile(channel_name):
 def parseChat(line, channel):
 	global message_count
 
-	timestamp = str(datetime.datetime.now()).split(".")[0]
-	username = getUser(line)
-	message = getMessage(line)
-	channelname = getChannelname(line)
-	owner = getOwner(line)
-	mod = getMod(line)
-	sub = getSub(line)
-	subbadge = getSubbadge(line)
-	turbo = getTurbo(line)
-	prime = getPrime(line)
+	if "PRIVMSG" in line:
+
+		message_name = "message-" + str(message_count)
+		timestamp = str(datetime.datetime.now()).split(".")[0]
+		username = getUser(line)
+		message = getMessage(line)
+		owner = getOwner(line)
+		mod = getMod(line)
+		sub = getSub(line)
+		subbadge = getSubbadge(line)
+		turbo = getTurbo(line)
+		prime = getPrime(line)
+		print("[READ MESSAGE]:\t" + timestamp + "\t" + username + "\t" + message)
 
 	message_count += 1
 
@@ -51,17 +54,21 @@ def parseChat(line, channel):
 	if username.endswith("bot"):
 		print("[NOTIFICATION]: A Bot sent a message!")
 		return
-	
-	print("[READ]:\t" + timestamp + "\t" + username + "\t" + message)
+
 	# print("[NOTIFICATION]: Adding message to Dictionary")
 	# addToDictionary(message_count, username, timestamp, message)
 
 
-def addToDictionary(message_id, username, timestamp, message):
+def addToDictionary(message_name, message_id, username, timestamp, message):
 	'''  '''
-	message_name = "message-" + str(message_id)
 	chat_log[message_name] = {
 		'id': message_id,
+		'owner': owner,
+		'mod': mod,
+		'sub': sub,
+		'subbadge': subbadge,
+		'turbo': turbo,
+		'prime': prime,
 		'username': username,
 		'timestamp': timestamp,
 		'message': message
